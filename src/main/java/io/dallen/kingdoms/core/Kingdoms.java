5fc72b6d9c9ce179e280b4e6cf091dc08adcec34
DonoA <me@donovanallen.net>
2017-05-18 21:18:47 -0600

    Refactor for rebuild

---
+++ src/main/java/io/dallen/kingdoms/core/Kingdoms.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2016 Donovan Allen.
+ * 
+ * This file is part of Kingdoms for the Morphics Network.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * 
+ */
+package io.dallen.kingdoms.core;
+
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import lombok.Getter;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author Donovan Allen
+ */
+public class Kingdoms extends JavaPlugin {
+
+    @Getter
+    private static Kingdoms plugin;
+
+    @Getter
+    private static ProtocolManager protocolManager;
+
+    @Override
+    public void onEnable() {
+        plugin = this;
+        protocolManager = ProtocolLibrary.getProtocolManager();
+        
+    }
+}
